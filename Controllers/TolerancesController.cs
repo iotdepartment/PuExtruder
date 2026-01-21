f3d94f719aff52294f26ea0f4915f0bc1869ab1c
lorlando <lorlando@RMX-FJMS624>
2026-01-21 13:05:17 -0600

    Delete and Edit Mandrels

--- Controllers/TolerancesController.cs
+++ Controllers/TolerancesController.cs
@@ -38,5 +38,71 @@ namespace WebApplication4.Controllers
 
             return PartialView("_DetalleTolerances", item);
         }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var entity = _context.TOLERANCES.FirstOrDefault(x => x.ID == id);
+
+            if (entity == null)
+                return NotFound();
+
+            _context.TOLERANCES.Remove(entity);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GetById(int id)
+        {
+            var entity = _context.TOLERANCES.FirstOrDefault(x => x.ID == id);
+
+            if (entity == null)
+                return NotFound();
+
+            return Json(entity);
+        }
+
+        [HttpPost]
+        public IActionResult Update(TOLERANCES model)
+        {
+            var entity = _context.TOLERANCES.FirstOrDefault(x => x.ID == model.ID);
+
+            if (entity == null)
+                return NotFound();
+
+            entity.EXTRUDER = model.EXTRUDER;
+            entity.MANDRIL = model.MANDRIL;
+            entity.FAMILIA = model.FAMILIA;
+
+            entity.ID_ = model.ID_;
+            entity.ID_TOL = model.ID_TOL;
+
+            entity.LONGITUD_CORTE = model.LONGITUD_CORTE;
+            entity.LONGITUD_CORTE_TOL = model.LONGITUD_CORTE_TOL;
+
+            entity.PARED = model.PARED;
+            entity.PARED_TOL = model.PARED_TOL;
+
+            entity.PITCH = model.PITCH;
+            entity.PITCH_TOL = model.PITCH_TOL;
+
+            entity.INNER_YARN = model.INNER_YARN;
+            entity.INNER_YARN_TOL = model.INNER_YARN_TOL;
+
+            entity.OUTER_YARN = model.OUTER_YARN;
+            entity.OUTER_YARN_TOL = model.OUTER_YARN_TOL;
+
+            entity.LONGITUD_LEYENDA = model.LONGITUD_LEYENDA;
+            entity.LONGITUD_LEYENDA_TOL = model.LONGITUD_LEYENDA_TOL;
+
+            entity.GROSOR_LEYENDA = model.GROSOR_LEYENDA;
+            entity.GROSOR_LEYENDA_TOL = model.GROSOR_LEYENDA_TOL;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
     }
 }
